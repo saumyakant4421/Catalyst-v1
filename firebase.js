f1045d0ea8507b19fc269098f1358b8f92aecd1e
Saumya Kant <saumyakant4421@gmail.com>
2025-01-03 23:24:01 +0530

    Deployment changes

--- firebase.js
+++ firebase.js
@@ -11,7 +11,19 @@ const { getStorage } = require("firebase/storage");
 
 // Initialize Firebase Admin SDK (for backend)
 if (admin.apps.length === 0) {
-  const serviceAccount = require('./config/catalyst-1d0af-firebase.json');
+  const serviceAccount = {
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), // Ensure newlines are preserved
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_CERT_URL,
+    universe_domain: process.env.UNIVERSE_DOMAIN,
+  };
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // Firebase storage bucket
